208728113145f6a6c31fb0d5b4753cc6176a05a6
Zach Jagoda <jagod101@mail.chapman.edu>
2020-03-04 00:16:36 -0800

    Intro to Unity #1 - Beginner Project [IN PROGRESS]

--- IntroToUnity/BeginnerProject/Assets/Scripts/PlayerController.cs
+++ IntroToUnity/BeginnerProject/Assets/Scripts/PlayerController.cs
@@ -3,21 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    [HEADER("Player One")]                              //Hierarchy Attribute for the Inspector
     //<-------------------------------- Player -------------------------------->
     public PlayerController Player1;                    //Instance of the Player Controller on the Player
     private Rigidbody2D rb;                             //Instance of the Rigidbody Object on the Player
     private Animator anim;                              //Instance of the Animator on the Player
     [Space]
-
-    [HEADER("Player Movement Logic")]                   //Hierarchy Attribute for the Inspector
+    [Header("Movement Logic")]                   //Hierarchy Attribute for the Inspector
     //<-------------------------------- Player Movement -------------------------------->
     private float mvSpeed = 5f;                         //Default Player Move Speed
-    public float horizontalMove = 0f;
+    public float horizontalMove;
     private Vector3 velocity;                           //Vector in which the Player moves
     [Space]
 
-    [HEADER("Player Jump Logic")]                       //Hierarchy Attribute for the Inspector
+    [Header("Jump Logic")]                       //Hierarchy Attribute for the Inspector
     //<-------------------------------- Player Jump -------------------------------->
     public float jumpForce = 150f;                      //Default Player Jump Force
     private bool grounded;                              //Boolean to set Player True/False depending on if grounded
@@ -28,7 +26,7 @@ public class PlayerController : MonoBehaviour {
     void Awake() {
         rb = GetComponent<Rigidbody2D>();               //On Awake, grab the Rigidbody2D Component
         anim = GetComponent<Animator>();                //On Awake, grab the Animator Component
-        Player1 = GetComponent<PlayerController>();     //On Awake, grab the Player Controller 
+        Player1 = GetComponent<PlayerController>();     //On Awake, grab the Player Controller
     }
 
     //<-------------------------------- Update -------------------------------->
@@ -40,7 +38,7 @@ public class PlayerController : MonoBehaviour {
         if(grounded && Input.GetKeyDown("Space")) {
             grounded = false;
             rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
-            
+
         }
         anim.SetFloat("Speed", velocity.sqrMagnitude);
     }
